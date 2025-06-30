52c9ddfeec37a43832e415ad2a6c2d90dded41dd
Abhishek Mishra <abhishekmishra0513@gmail.com>
2025-06-30 17:47:03 +0530

    Initial Commit -01

---
+++ src/main/java/com/example/spring_docker_with_Database/SpringDockerWithDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_docker_with_Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerWithDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerWithDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_docker_with_Database/SpringDockerWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_docker_with_Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
